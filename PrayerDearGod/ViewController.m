86be0216f7d89ffebd1b33d8a5d3ee9f8be3b9c5
iOS Camp <ioscamp@e4-ce-8f-2a-63-5a.wlan.lsu.edu>
2012-08-10 15:21:34 -0500

    Initial Commit

--- PrayerDearGod/ViewController.m
+++ PrayerDearGod/ViewController.m
@@ -13,15 +13,48 @@
 @end
 
 @implementation ViewController
+@synthesize display;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *prayerFilePath = @"PrayerFile_";
+    
+    NSDate *currentDate = [NSDate date];
+
+    NSDateComponents *currentDateComponents =
+    [[NSCalendar currentCalendar] components:(NSDayCalendarUnit | NSMonthCalendarUnit | NSYearCalendarUnit) fromDate:currentDate];
+        
+   prayerFilePath =  [prayerFilePath stringByAppendingFormat:@"%d%02d%02d", [currentDateComponents year],[currentDateComponents month], [currentDateComponents day]];     
+     
+    //[currentDateComponents year],[currentDateComponents month], [currentDateComponents date]];
+    
+    NSLog(@"currentDateComponents: %@", currentDateComponents);
+    
+    NSLog(@"prayerFilePath: %@", prayerFilePath);
+
+    NSString *stringPath = [[NSBundle mainBundle] pathForResource:prayerFilePath  ofType:@"html"];
+    
+    if (!stringPath) {
+          stringPath = [[NSBundle mainBundle] pathForResource:@"PrayerFile_default"  ofType:@"html"];
+    }
+    
+    NSURL *fileURL = [[NSURL alloc] initFileURLWithPath:stringPath];
+    
+    
+    
+    NSURLRequest *prayerdeargod = [NSURLRequest requestWithURL:fileURL];
+    
+    [display loadRequest:prayerdeargod]; 
+    
+    
 }
 
 - (void)viewDidUnload
 {
+    [self setDisplay:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
